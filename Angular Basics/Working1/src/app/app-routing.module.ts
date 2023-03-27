460d3fe853e6ce412af951075c38027762d6dfcf
Pratap_Singh_Sisodia <prataps135@gmail.com>
2023-03-27 17:07:32 +0530

    27 March 2023

--- Angular Basics/Working1/src/app/app-routing.module.ts
+++ Angular Basics/Working1/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevelopmentComponent } from './development/development.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ServiceComponent } from './service/service.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TestingComponent } from './testing/testing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent,
+  },
+  {
+    path:'development',component:DevelopmentComponent
+  },
+  {
+    path:'service',component:ServiceComponent
+  },
+  {
+    path:'testing',component:TestingComponent
+  },
+  {
+    path:'sign_up',component:SignUpComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular Basics/Working1/src/app/app.module.ts
+++ Angular Basics/Working1/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DevelopmentComponent } from './development/development.component';
+import { TestingComponent } from './testing/testing.component';
+import { ServiceComponent } from './service/service.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DevelopmentComponent,
+    TestingComponent,
+    ServiceComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
