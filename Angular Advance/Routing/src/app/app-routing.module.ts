4060a9c9a7316801550c889eab3fa0e27b300ae8
Pratap_Singh_Sisodia <prataps135@gmail.com>
2023-05-21 10:26:45 +0530

    20 May 2023

--- Angular Advance/Routing/src/app/app-routing.module.ts
+++ Angular Advance/Routing/src/app/app-routing.module.ts
@@ -4,17 +4,26 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { UsersComponent } from './components/users/users.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
-  {path:'about',component:AboutUsComponent},
-  {path:'blog',component:BlogComponent},
-  {path:'',component:HomeComponent},
-  {path:'users',component:UsersComponent},
-  {path:'#',component:HomeComponent}
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'blog', component: BlogComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':userId', component: UserDetailsComponent },
+      { path: '', component: PlaceholderComponent }
+    ]
+  },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
+  providers: [AuthGuard],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- Angular Advance/Routing/src/app/components/user-details/user-details.component.ts
+++ Angular Advance/Routing/src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit{
+  users: Array<{ id: number, name: string, email: string }> = [
+    { id: 1, name: "Leanne Graham", email: "Sincere@april.biz" },
+    { id: 2, name: "Ervin Howell", email: "Shanna@melissa.tv" },
+    { id: 3, name: "Clementine Bauch", email: "Nathan@yesenia.net" },
+    { id: 4, name: "Patricia Lebsack", email: "Julianne.OConner@kory.org" },
+    { id: 5, name: "Chelsey Dietrich", email: "Lucio_Hettinger@annie.ca" },
+    { id: 6, name: "Mrs. Dennis Schulist", email: "Karley_Dach@jasper.info" },
+    { id: 7, name: "Kurtis Weissnat", email: "Telly.Hoeger@billy.biz" },
+    { id: 8, name: "Nicholas Runolfsdottir V", email: "Sherwood@rosamond.me" },
+    { id: 9, name: "Glenna Reichert", email: "Chaim_McDermott@dana.io" },
+    { id: 10, name: "Clementina DuBuque", email: "Rey.Padberg@karina.biz" }
+  ];
+  
+  user:any;
 
+  constructor(private activatedRoute :ActivatedRoute){
+
+  }
+
+  ngOnInit(){
+    this.activatedRoute.params.subscribe((params)=>{
+       this.user = this.users.filter((user)=>{
+        { return user.id === +params['userId']}
+      })[0];
+    });
+
+    this.activatedRoute.queryParams.subscribe((qs)=>{
+      console.log("get the querry string as"+qs); 
+    });
+  }
 }
