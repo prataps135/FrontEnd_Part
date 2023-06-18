89af686dfe51994a63108834bb0e1a413c90981e
Pratap_Singh_Sisodia <prataps135@gmail.com>
2023-06-18 17:54:58 +0530

    form completed

--- Angular Advance/simple_template_driven_form/src/app/app.module.ts
+++ Angular Advance/simple_template_driven_form/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
